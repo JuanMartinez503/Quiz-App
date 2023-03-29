e9ce4e4b5801113037b3c04d59bca6241b234350
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-03-29 17:37:38 -0400

    finished with most of my code

--- game.js
+++ game.js
@@ -1,193 +1,217 @@
-
-const timerEl = document.getElementById('timer');
-const questionContainer = document.querySelector('.question-container');
-const questionEl =document.querySelector('.question');
-const choicesEL = document.querySelectorAll('.choices');
-const result = document.getElementById('result');
+const timerEl = document.getElementById("timer");
+const questionContainer = document.querySelector(".question-container");
+const questionEl = document.querySelector(".question");
+const choicesEL = document.querySelectorAll(".choices");
+const result = document.getElementById("result");
+const formEl = document.querySelector("#form");
+// Here are all og my document selectors used
+var timerSet;
 
 let timer = 50;
-let questionNumber =0;
 
- let question1 = {
-    question: "What is the output of the following code?: console.log(typeof [])",
-    answerOptions: ["object", "array", "undefined", "string"],
-    correctAnswer: "object"
-}
+let question1 = {
+  question: "What is the output of the following code?: console.log(typeof [])",
+  answerOptions: ["object", "array", "undefined", "string"],
+  correctAnswer: "object",
+};
 let question2 = {
-    question: "Which of the following is not a JavaScript data type?",
-    answerOptions: ["number", "boolean", "null", "table"],
-    correctAnswer: "table"
-}
-let question3 = 
-    {
-        question: "What does DOM stand for?",
-        answerOptions: ["Document Orientation Model", "Digital Ordinance Model", "Data Object Mode", "Document Object Model"],
-        correctAnswer: "Document Object Model"
-    }
-    
+  question: "Which of the following is not a JavaScript data type?",
+  answerOptions: ["number", "boolean", "null", "table"],
+  correctAnswer: "table",
+};
+let question3 = {
+  question: "What does DOM stand for?",
+  answerOptions: [
+    "Document Orientation Model",
+    "Digital Ordinance Model",
+    "Data Object Mode",
+    "Document Object Model",
+  ],
+  correctAnswer: "Document Object Model",
+};
+
 let question4 = {
-    question: "What is the difference between let and var in JavaScript?",
-    answerOptions: ["var is block-scoped and let is function-scoped", "let is block-scoped and var is function-scoped", "let is only used for loops and var is used for variables", "there is no difference between the two"],
-    correctAnswer: "let is block-scoped and var is function-scoped"
+  question: "What is the difference between let and var in JavaScript?",
+  answerOptions: [
+    "var is block-scoped and let is function-scoped",
+    "let is block-scoped and var is function-scoped",
+    "let is only used for loops and var is used for variables",
+    "there is no difference between the two",
+  ],
+  correctAnswer: "let is block-scoped and var is function-scoped",
+};
+// I made a functions for every question
+
+function startGame() {
+  setTimer();
+  // I made a timer function to set a timer for the startGame()
+
+  question1Func(); //I also ran this function which runs all of the other functions after the question is answered
 }
 
-function startGame (){
-    
-    setTimer()
-    
-    question1Func()
-    
-    
+function question1Func() {
+  questionEl.innerText = question1.question;
+
+  choicesEL[0].innerText = question1.answerOptions[0];
+  choicesEL[1].innerText = question1.answerOptions[1];
+  choicesEL[2].innerText = question1.answerOptions[2];
+  choicesEL[3].innerText = question1.answerOptions[3];
+  for (let i = 0; i < choicesEL.length; i++) {
+    choicesEL[i].setAttribute("style", "width:150px;");
+  }//I attached the event listener to my container so it would identify the answer by the data-type
+  questionContainer.addEventListener("click", function (e) {
+    var element = e.target;
+    var type = element.getAttribute("data-type");
+    if (type == 1) {
+      result.innerText = "Correct!";
+      result.setAttribute(
+        "style",
+        " color: green; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+      );
+    } else {
+      result.innerText = "Wrong!";
+      result.setAttribute(
+        "style",
+        " color: red; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+      );
+      timer = timer - 5;
+      // I subtracted 5 to the timer every time the question is wrong
+    }
+    question2Func();
+  });
 }
 
-function question1Func(){
-        
-        questionEl.innerText = question1.question
-        choicesEL[0].innerText =question1.answerOptions[0]
-        choicesEL[1].innerText =question1.answerOptions[1]
-        choicesEL[2].innerText =question1.answerOptions[2]
-        choicesEL[3].innerText =question1.answerOptions[3]
-        for(let i = 0; i <choicesEL.length; i++){
-            choicesEL[i].setAttribute('style', 'width:150px;')
-        }
-        questionContainer.addEventListener('click', function(e){
-            var element = e.target
-            var type = element.getAttribute('data-type')
-            if(type ==1){
-                result.innerText= 'Correct!'
-            }
-            else{
-                result.innerText= 'Wrong'
-                timer = timer-4;
-
-            }
-    question2Func()
-            
-        })
-
+function question2Func() {
+  questionEl.innerText = question2.question;
+  choicesEL[0].innerText = question2.answerOptions[0];
+  choicesEL[1].innerText = question2.answerOptions[1];
+  choicesEL[2].innerText = question2.answerOptions[2];
+  choicesEL[3].innerText = question2.answerOptions[3];
+  for (let i = 0; i < choicesEL.length; i++) {
+    choicesEL[i].setAttribute("style", "width:150px;");
+  }
+  questionContainer.addEventListener("click", function (e) {
+    var element = e.target;
+    var type = element.getAttribute("data-type");
+    if (type == 4) {
+      result.innerText = "Correct!";
+      result.setAttribute(
+        "style",
+        " color: green; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+      );
+    } else {
+      result.innerText = "Wrong!";
+      result.setAttribute(
+        "style",
+        " color: red; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+      );
+      timer;
     }
-    
-       function question2Func() {
-        questionEl.innerText = question2.question
-        choicesEL[0].innerText =question2.answerOptions[0]
-        choicesEL[1].innerText =question2.answerOptions[1]
-        choicesEL[2].innerText =question2.answerOptions[2]
-        choicesEL[3].innerText =question2.answerOptions[3]
-        for(let i = 0; i <choicesEL.length; i++){
-            choicesEL[i].setAttribute('style', 'width:150px;')
-        }
-        questionContainer.addEventListener('click', function(e){
-            var element = e.target
-            var type = element.getAttribute('data-type')
-            if(type ==4){
-                result.innerText= 'Correct!'
-            }
-            else{
-                result.innerText= 'Wrong'
-                timer 
-                
-            }
-        question3Func()
-        })
-    function question3Func(){
-        questionEl.innerText = question3.question
-        choicesEL[0].innerText =question3.answerOptions[0]
-        choicesEL[1].innerText =question3.answerOptions[1]
-        choicesEL[2].innerText =question3.answerOptions[2]
-        choicesEL[3].innerText =question3.answerOptions[3]
-        for(let i = 0; i <choicesEL.length; i++){
-            choicesEL[i].setAttribute('style', 'width:250px;')
-        }
-        questionContainer.addEventListener('click', function(e){
-            var element = e.target
-            var type = element.getAttribute('data-type')
-            if(type ==4){
-                result.innerText= 'Correct!'
-            }
-            else{
-                result.innerText= 'Wrong'
-                timer
-
-            }
-            question4Func()
-        })
+    question3Func();
+  });
+  function question3Func() {
+    questionEl.innerText = question3.question;
+    choicesEL[0].innerText = question3.answerOptions[0];
+    choicesEL[1].innerText = question3.answerOptions[1];
+    choicesEL[2].innerText = question3.answerOptions[2];
+    choicesEL[3].innerText = question3.answerOptions[3];
+    for (let i = 0; i < choicesEL.length; i++) {
+      choicesEL[i].setAttribute("style", "width:250px;");
     }
-        function question4Func(){
-            questionEl.innerText = question4.question
-            choicesEL[0].innerText =question4.answerOptions[0]
-            choicesEL[1].innerText =question4.answerOptions[1]
-            choicesEL[2].innerText =question4.answerOptions[2]
-            choicesEL[3].innerText =question4.answerOptions[3]
-            for(let i = 0; i <choicesEL.length; i++){
-                choicesEL[i].setAttribute('style', 'width:250px;')
-            }
-            questionContainer.addEventListener('click', function(e){
-                var element = e.target
-                var type = element.getAttribute('data-type')
-                if(type ==2){
-                    result.innerText= 'Correct!'
-                }
-                else{
-                    result.innerText= 'Wrong'
-                    timer 
-
-                }
-                clearInterval(timerSet)
-                initials()
-
-            })
-        }
+    questionContainer.addEventListener("click", function (e) {
+      var element = e.target;
+      var type = element.getAttribute("data-type");
+      if (type == 4) {
+        result.innerText = "Correct!";
+        result.setAttribute(
+          "style",
+          " color: green; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+        );
+      } else {
+        result.innerText = "Wrong!";
+        result.setAttribute(
+          "style",
+          " color: red; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+        );
+        timer;
+      }
+      question4Func();
+    });
+  }
+  function question4Func() {
+    questionEl.innerText = question4.question;
+    choicesEL[0].innerText = question4.answerOptions[0];
+    choicesEL[1].innerText = question4.answerOptions[1];
+    choicesEL[2].innerText = question4.answerOptions[2];
+    choicesEL[3].innerText = question4.answerOptions[3];
+    for (let i = 0; i < choicesEL.length; i++) {
+      choicesEL[i].setAttribute("style", "width:250px;");
     }
- function initials() {
-  choicesEL.forEach(choice => {
-    choice.remove()
-  })
-  result.remove()
-  questionEl.innerHTML = 'Enter your initials here to save your score!'
+    questionContainer.addEventListener("click", function (e) {
+      var element = e.target;
+      var type = element.getAttribute("data-type");
+      if (type == 2) {
+        result.innerText = "Correct!";
+        result.setAttribute(
+          "style",
+          " color: green; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+        );
+      } else {
+        result.innerText = "Wrong!";
+        result.setAttribute(
+          "style",
+          " color: red; margin-top: 2%; border-top: 2px solid black;font-size: 20px;width: 100%;"
+        );
+        timer;
+      }
+      initials();
+      clearInterval(timerSet);
+      // I clear the interval when the last question is answered.
 
-  let form = document.createElement('form')
-  let input = document.createElement('input')
-  let button = document.createElement('button')
+    });
+  }
+}
+function initials() {
+  choicesEL.forEach((choice) => {
+    choice.remove();
+  });
+  result.remove();
+  questionEl.innerHTML = `Enter your initials here to save your score!Your final score is ${timer}`;
 
-  input.addEventListener('submit', function(event) {
-    event.preventDefault() // Prevent the default form submission behavior
+  let input = document.createElement("input");
+  let button = document.createElement("button");
+  button.innerText = "Submit";
 
+  formEl.append(input); // append the input field to the form element
+  formEl.append(button);
 
+  button.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    console.log("hello");
     // Handle the submission data or redirect to another page
-  })
-  questionEl.append(form)
- 
-  form.append(input) // append the input field to the form element
-  form.append(button)
-  
+    let userInitials = {
+      initials: input.value,
+      score: timer,
+    };
 
-
-
-  button.innerHTML = 'Submit'
-
-  let userInitials = {
-    initials : input.value,
-    score : timer
-}
-localStorage.setItem('userInitials', JSON.stringify(userInitials))
-button.addEventListener('click', function(){
-    window.location='index.html'
-})
+    localStorage.setItem("userInitials", JSON.stringify(userInitials));
+    window.location = "scores.html";
+    // This pushes the page to the scores.html
+  });
 }
 
-      
-
-    function setTimer(){
-        timer--
-    timerEl.innerText = `Timer: ${timer}`
-    if(timer<0){
-        clearInterval(timerSet)
-    timerEl.innerText = `Timer: 0`
-    return startGame()
-
+function setTimer() {
+  timerSet = setInterval(function () {
+    timer--;
+    timerEl.innerText = `Timer: ${timer}`;
+    if (timer === 0) {
+      // the interval is cleared if the timer is 0
+      clearInterval(timerSet);
+      timer = 0;
+      timerEl.innerText = `Timer: 0`;
+      initials();
     }
+  }, 1000);
 }
-var timerSet= setInterval(setTimer,1000)
 
-  
-startGame()
+startGame();
---
+++ scores.js
@@ -0,0 +1,16 @@
+const list = document.getElementById('list');
+var stringKey = JSON.parse(localStorage.getItem('userInitials'));
+
+if (stringKey !== null) {
+  for (var i = 0; i < stringKey.length; i++) {
+    var liEL = document.createElement('li');
+    liEL.innerText = `Initials: ${stringKey[i].initials} Score: ${stringKey[i].score}`;
+    list.appendChild(liEL);
+  }
+}
+
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', function() {
+  localStorage.removeItem('userInitials');
+  list.innerHTML = ''; // Clear the displayed list
+})
